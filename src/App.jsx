9d8970ef2b308a1bf6c53303a8bd5464419a1183
Nur Mohammod <nurm98371@gmail.com>
2025-07-28 20:52:23 +0600

    Knoeledge cafe

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+      <h1 className='bg-neutral-500 text-orange-500 text-3xl text-center'>Knowledge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
